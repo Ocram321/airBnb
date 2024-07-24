5a218126817a9a7d05b0b5e833807f319afeca9a
Marco Milinovich <ocram321@gmail.com>
2024-07-24 14:56:26 -0700

    testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,10 +7,7 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+
 
 
 // backend/routes/index.js
