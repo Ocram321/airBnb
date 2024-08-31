c9feb7de1268c35b2c8648b6388ace9314c08a51
Marco Milinovich <ocram321@gmail.com>
2024-08-31 16:24:30 -0700

    formatting code for legibility

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,23 +2,17 @@
 const router = require('express').Router();
 
 const { restoreUser } = require('../../utils/auth.js');
-
 const { requireAuth } = require('../../utils/auth.js');
 
-router.use(restoreUser);
-
-  // GET /api/set-token-cookie
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
-// const { requireAuth } = require('../../utils/auth.js');
+
 
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
+  router.use(restoreUser);
 
-  router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-  });
 
 module.exports = router;
