ae9b2a85759e6c6199135e77fdb0fb15cf4d1b17
Marco Milinovich <ocram321@gmail.com>
2024-10-16 20:16:35 -0700

    added review and reviewimage seeders

---
+++ backend/db/seeders/20241017025724-reviewImage.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const reviewimage = require('../models/reviewimage');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await reviewimage.bulkCreate([
+      {
+        reviewId: 1,
+        url: 'www.randompicture.com'
+      },
+      {
+        reviewId: 2,
+        url: 'www.randompicture2.com'
+      },
+      {
+        reviewId: 3,
+        url: 'www.randompicture3.com'
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      reviewId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
