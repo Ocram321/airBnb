e54c28c10002601b0e6db88d0d9b9c44949d5545
Collin Ehret <collin.ehret@gmail.com>
2024-10-17 18:51:43 -0700

    review and review image seeds fixed

--- backend/db/seeders/20241017025724-reviewImage.js
+++ backend/db/seeders/20241017025724-reviewImage.js
@@ -1,52 +1,42 @@
-'use strict';
-
-const reviewimage = require('../models/reviewimage');
+"use strict";
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+    options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert(
+            "ReviewImages",
+            [
+                {
+                    reviewId: 1,
+                    url: "www.randompicture.com",
+                },
+                {
+                    reviewId: 2,
+                    url: "www.randompicture2.com",
+                },
+                {
+                    reviewId: 3,
+                    url: "www.randompicture3.com",
+                },
+            ],
+            options
+        );
+    },
 
-    await reviewimage.bulkCreate([
-      {
-        reviewId: 1,
-        url: 'www.randompicture.com'
-      },
-      {
-        reviewId: 2,
-        url: 'www.randompicture2.com'
-      },
-      {
-        reviewId: 3,
-        url: 'www.randompicture3.com'
-      }
-    ])
-  },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    options.tableName = 'Users';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      reviewId: { [Op.in]: [1, 2, 3] }
-    }, {});
-  }
+    async down(queryInterface, Sequelize) {
+        options.tableName = "ReviewImages";
+        const Op = Sequelize.Op;
+        return queryInterface.bulkDelete(
+            options,
+            {
+                reviewId: { [Op.in]: [1, 2, 3] },
+            },
+            {}
+        );
+    },
 };
--- backend/db/seeders/20241017030815-demo-review.js
+++ backend/db/seeders/20241017030815-demo-review.js
@@ -1,56 +1,46 @@
-'use strict';
-
-const review = require('../models/review');
-
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+    options.schema = process.env.SCHEMA; // define your schema in options object
 }
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    await review.BulkCreate([
-      {
-        spotId: 1,
-        userId: 1,
-        review: "This is my reveiw ",
-        stars: 4
-      },
-      {
-        spotId: 2,
-        userId: 2,
-        review: "This is my reveiw for spot 2 ",
-        stars: 5
-      },
-      {
-        spotId: 3,
-        userId: 1,
-        review: "This is my reveiw for spot 3",
-        stars: 3
-      },
-    ])
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert(
+            "Reviews",
+            [
+                {
+                    spotId: 1,
+                    userId: 1,
+                    review: "This is my review",
+                    stars: 4,
+                },
+                {
+                    spotId: 2,
+                    userId: 2,
+                    review: "This is my review for spot 2",
+                    stars: 5,
+                },
+                {
+                    spotId: 3,
+                    userId: 1,
+                    review: "This is my review for spot 3",
+                    stars: 3,
+                },
+            ],
+            options
+        );
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    options.tableName = 'Users';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3] }
-    }, {});
-  }
+    async down(queryInterface, Sequelize) {
+        options.tableName = "Reviews";
+        const Op = Sequelize.Op;
+        return queryInterface.bulkDelete(
+            options,
+            {
+                spotId: { [Op.in]: [1, 2, 3] },
+            },
+            {}
+        );
+    },
 };
