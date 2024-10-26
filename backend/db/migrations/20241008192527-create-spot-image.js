e18768b6c00ca84b3476d0ee0a8eaf1bc33df713
davidzon <davidson18ramirez@gmail.com>
2024-10-25 23:46:44 -0400

    fixed migrations down function

--- backend/db/migrations/20241008192527-create-spot-image.js
+++ backend/db/migrations/20241008192527-create-spot-image.js
@@ -36,6 +36,7 @@ module.exports = {
     },options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('SpotImages', options);
+    options.tableName = "SpotImages";
+    await queryInterface.dropTable(options);
   }
 };
