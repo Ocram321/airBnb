a8f7a6272202932889263711582a3db1d4365c3d
Collin Ehret <collin.ehret@gmail.com>
2024-10-26 16:20:04 -0700

    more delete review im cahnges

--- backend/routes/api/reviewimages.js
+++ backend/routes/api/reviewimages.js
@@ -31,7 +31,7 @@ router.delete("/:imageId", requireAuth, async (req, res, next) => {
 
         await reviewImage.destroy();
 
-        return res.status(200).json({
+        res.status(200).json({
             message: "Successfully deleted",
         });
     } catch (error) {
