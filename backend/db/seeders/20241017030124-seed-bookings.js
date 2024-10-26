37f5f0c5f7e272660cf6e8dac09c6f3cd0908101
Marco Milinovich <ocram321@gmail.com>
2024-10-26 10:00:25 -0700

    aactually fixed booking error

--- backend/db/seeders/20241017030124-seed-bookings.js
+++ backend/db/seeders/20241017030124-seed-bookings.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = "Bookings";
-    await queryInterface.bulkInsert('Bookings', [
+    await queryInterface.bulkInsert(options, [
       {
         spotId: 1,
         userId: 1,
